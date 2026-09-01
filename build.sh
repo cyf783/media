b4566b53806d8c7c17a3be7d41a7d19f298d1f73
Swifly <cyf783@qq.com>
2026-09-01 18:16:39 +0800

    ci: add build script with JAVA_HOME/ANDROID_HOME defaults

--- build.sh
+++ build.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -e
 
+export JAVA_HOME="${JAVA_HOME:-/Users/swifly/.sdkman/candidates/java/current}"
+export ANDROID_HOME="${ANDROID_HOME:-/Users/swifly/Library/Android/sdk}"
+
 echo "=== 清理残留 Gradle 文件 ==="
 rm -f ~/.gradle/wrapper/dists/gradle-9.1.0-all/7wzd0jkjit61aq2p43wpjgij9/gradle-9.1.0-all.zip.lck
 rm -f ~/.gradle/wrapper/dists/gradle-9.1.0-all/7wzd0jkjit61aq2p43wpjgij9/gradle-9.1.0-all.zip.part 2>/dev/null || true
